8837a9a9e27f1682f92c05b99feac24d8dadce2d
Your Name <you@example.com>
2024-03-21 20:44:19 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,7 +43,7 @@ Al finalizar retorna la lista creada.
 List *crea_lista() {
   List *L = create_list();
   for (int i = 0; i < 10; i++) {
-    int *dato(malloc(sizeof(int)));
+    int *dato = (int *)malloc(sizeof(int));
     *dato = i + 1;
     push_back(L, dato);
   }
